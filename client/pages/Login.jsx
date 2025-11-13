f2205dc2e85a7e5a1722a41b84c4ec38ad7c467c
Idan Bahar <bahar.idan91@gmail.com>
2025-11-13 13:16:00 +0200

    converting css to scss with nesting classes

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { authAPI } from '../utils/api'
-import '../src/assets/styles/Login.css'
+import '../src/assets/styles/Login.scss'
 
 function Login() {
   const navigate = useNavigate()
